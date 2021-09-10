30279bc0e62aaa993e18fb48f57a46fff9161112
Jordan-Shelton <jordansheltonwork@gmail.com>
2021-09-10 09:28:32 -0500

    finished the mvp just need to tackle the interview questions

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import React, { Component } from "react";
 import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
 import Header from './components/Header';
-
-import axios from 'axios';
-
+import { fetchSmurfs } from './actions/'
+import { connect } from 'react-redux';
+import { Form } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 class App extends Component {
-  componentDidMount() {
-    axios.get('http://localhost:3333/smurfs')
-    .then(res => console.log(res))
-    .catch(err => console.log('Axios Error', err));
+  componentDidMount(){
+    this.props.fetchSmurfs()
   }
-
   render() {
     return (
       <div className="App">
@@ -30,7 +27,15 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return{
+    smurfs:state.smurfs,
+    isLoading:state.isLoading,
+    error:state.erroe,
+  }
+}
+
+export default connect(mapStateToProps, { fetchSmurfs })(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import "./index.css";
 import App from "./App";
+import reducer from './reducers'
 
 const { worker } = require('./mocks/browser');
 worker.start();
 
 const rootElement = document.getElementById("root");
 
+const store = createStore(reducer, applyMiddleware(thunk, logger))
+
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , 
     rootElement
 );
-
 //Task List:
 //1. Add in all necessary components and libary methods.
 //2. Create a store that includes thunk and logger middleware support.
